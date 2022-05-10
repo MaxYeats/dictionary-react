3e4fab4486e677e0261b37f17baf234888f9a34e
Max Yeats <90226233+MaxYeats@users.noreply.github.com>
2022-05-10 15:38:42 -0400

    Activated bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@
 
 import capy from "./Capybara1.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={capy} className="img-fluid capy-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={capy} className="img-fluid" alt="logo" id="capy-logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This is an open sourced page built by{" "}
+          <a
+            href="https://github.com/MaxYeats/dictionary-react"
+            title="link to Melissa's Github project page"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Melissa Tseng
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <form>
+        <input className="input-search"></input>
+        <button className="btn btn-primary">Look it up</button>
+      </form>
+
+      <div>Pai dos Burros</div>
+    </div>
+  );
+}
