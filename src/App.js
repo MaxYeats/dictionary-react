21f179e2c4dc28d301192d746e26bcc64b3306fd
Max Yeats <90226233+MaxYeats@users.noreply.github.com>
2022-05-09 21:48:51 -0400

    Basic structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import capy from "./Capybara1.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={capy} className="img-fluid capy-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
