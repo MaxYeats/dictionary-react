6ecb2bf8f8c4b68697cd51ddc7e8f4d2a658239d
Max Yeats <90226233+MaxYeats@users.noreply.github.com>
2022-05-10 19:56:36 -0400

    Added search engine
    -Added search engine Dictionary.js,
    -Added dummy icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import capy from "./Capybara1.png";
+import logo from "./soccerfield1.png";
 import "./App.css";
 import Dictionary from "./Dictionary.js";
 import "bootstrap/dist/css/bootstrap.css";
@@ -10,13 +10,13 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={capy} className="img-fluid" alt="logo" id="capy-logo" />
+          <img src={logo} className="img-fluid" alt="logo" id="logo" />
         </header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          This is an open sourced page built by{" "}
+          This is an open-sourced page built by{" "}
           <a
             href="https://github.com/MaxYeats/dictionary-react"
             title="link to Melissa's Github project page"
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,30 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <form>
-        <input className="input-search" autoComplete="false"></input>
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          className="input-search"
+          placeholder="Enter your word here"
+          autoComplete="false"
+          autoFocus={true}
+        />
         <button className="btn btn-primary">Look it up</button>
       </form>
 
